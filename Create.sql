8cce3da7114568227ea4f4576cac8d24488d8f94
SpiderInMyEar <Thomas.romeyer@gmail.com>
2023-01-08 14:07:01 +0100

    Added tables to create

--- Create.sql
+++ Create.sql
@@ -61,3 +61,48 @@ Constraint PK_SYMPTOME primary key (mal_code, obs_id),
 Constraint FK_SYMPTOME_MALADIE foreign key (mal_code) references maladie (code),
 Constraint FK_SYMPTOME_OBSERV foreign key (obs_id) references observation (id)
 );
+
+CREATE TABLE observe
+(obs_id NUMBER NOT NULL,
+con_id NUMBER NOT NULL,
+Constraint PK_OBSERVE primary key (con_id, obs_id),
+Constraint FK_OBSERVE_CONS foreign key (con_id) references consultation (id),
+Constraint FK_OBSERVE_OBSERV foreign key (obs_id) references observation (id)
+);
+
+CREATE TABLE chronique
+(n_pat NUMBER NOT NULL,
+mal_code VARCHAR(30) NOT NULL,
+Constraint PK_CHRONIQUE primary key (n_pat, mal_code),
+Constraint FK_CHRONIQUE_PATIENT foreign key (n_pat) references patient (n_pat) ON DELETE CASCADE,
+Constraint FK_CHRONIQUE_MALADIE foreign key (mal_code) references maladie (code)
+);
+
+CREATE TABLE laboratoire_pharmaceutique
+(n_labo NUMBER NOT NULL,
+nom VARCHAR(60),
+adresse VARCHAR(30),
+Constraint PK_LABORATOIRE primary key (n_labo)
+);
+
+CREATE TABLE medicament
+(id NUMBER NOT NULL,
+nom VARCHAR(60),
+Constraint PK_MEDICAMENT primary key (id)
+);
+
+CREATE TABLE travaille
+(n_med NUMBER NOT NULL,
+n_labo NUMBER NOT NULL,
+Constraint PK_TRAVAILLE primary key (n_med, n_labo),
+Constraint FK_TRAVAILLE_MEDECIN foreign key (n_med) references medecin (n_med),
+Constraint FK_TRAVAILLE_LABO foreign key (n_labo) references laboratoire_pharmaceutique (n_labo)
+);
+
+CREATE TABLE developpe
+(n_med NUMBER NOT NULL,
+medic_id NUMBER NOT NULL,
+Constraint PK_DEVELOPPE primary key (n_med, medic_id),
+Constraint FK_DEVELOPPE_MEDECIN foreign key (n_med) references medecin (n_med),
+Constraint FK_DEVELOPPE_MEDICAMENT foreign key (medic_id) references medicament (id)
+);
