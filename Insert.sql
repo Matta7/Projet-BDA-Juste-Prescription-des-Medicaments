f8fcb0b2faa41b9ed7ea89a9a65cca731931132b
Mathieu Vallee <21910887@etu.unicaen.fr>
2023-01-18 12:13:41 +0100

    insert fix

--- Insert.sql
+++ Insert.sql
@@ -14,20 +14,20 @@ INSERT INTO patient (n_pat, Nom, Prenom) VALUES(1, 'Dupont', 'Jean');
 INSERT INTO patient (n_pat, Nom, Prenom) VALUES(2, 'Jiji', 'Léa');
 
 
-INSERT INTO medicament (id, nom, notice_url) VALUES (1, "Doliprane", NULL);
-INSERT INTO medicament (id, nom, notice_url) VALUES (2, "Efferalgan", NULL);
-INSERT INTO medicament (id, nom, notice_url) VALUES (3, "Dafalgan", NULL);
-INSERT INTO medicament (id, nom, notice_url) VALUES (4, "Levothyrox", NULL);
+INSERT INTO medicament (id, nom, notice_url) VALUES (1, 'Doliprane', NULL);
+INSERT INTO medicament (id, nom, notice_url) VALUES (2, 'Efferalgan', NULL);
+INSERT INTO medicament (id, nom, notice_url) VALUES (3, 'Dafalgan', NULL);
+INSERT INTO medicament (id, nom, notice_url) VALUES (4, 'Levothyrox', NULL);
 
 
-INSERT INTO effet_indesirable (id, text) VALUES (1, "Augmentation de la transpiration, une augmentation du flux sanguin cutané et une perte de chaleur.")
-INSERT INTO effet_indesirable (id, text) VALUES (2, "Augmentation de la transpiration, une augmentation du flux sanguin cutané et une perte de chaleur.")
-INSERT INTO effet_indesirable (id, text) VALUES (3, "Augmentation de la transpiration, une augmentation du flux sanguin cutané et une perte de chaleur.")
-INSERT INTO effet_indesirable (id, text) VALUES (4, "Augmentation du rythme cardiaque.")
+INSERT INTO effet_indesirable (id, text) VALUES (1, 'Augmentation de la transpiration, une augmentation du flux sanguin cutané et une perte de chaleur.')
+INSERT INTO effet_indesirable (id, text) VALUES (2, 'Augmentation de la transpiration, une augmentation du flux sanguin cutané et une perte de chaleur.')
+INSERT INTO effet_indesirable (id, text) VALUES (3, 'Augmentation de la transpiration, une augmentation du flux sanguin cutané et une perte de chaleur.')
+INSERT INTO effet_indesirable (id, text) VALUES (4, 'Augmentation du rythme cardiaque.')
 
 
-INSERT INTO substance_active (id, nom, descrip) VALUES (1, "Paracétamol", "Le paracétamol est un médicament utilisé dans le traitement des douleurs légères à modérées et/ou dans les états fébriles.");
-INSERT INTO substance_active (id, nom, descrip) VALUES (2, "Lévothyroxine sodique", "Hormone thyroïdienne, chimiquement définie sous sa forme naturelle L plus active que la forme D.");
+INSERT INTO substance_active (id, nom, descrip) VALUES (1, 'Paracétamol', 'Le paracétamol est un médicament utilisé dans le traitement des douleurs légères à modérées et/ou dans les états fébriles.');
+INSERT INTO substance_active (id, nom, descrip) VALUES (2, 'Lévothyroxine sodique', 'Hormone thyroïdienne, chimiquement définie sous sa forme naturelle L plus active que la forme D.');
 
 
 INSERT INTO effet_connus_s (id, subs_id) VALUES (1, 1);
@@ -48,22 +48,22 @@ INSERT INTO substance_contenu (medic_id, subs_id) VALUES (1, 3);
 INSERT INTO substance_contenu (medic_id, subs_id) VALUES (2, 4);
 
 
-INSERT INTO classe_pharmacologique (code, parent) VALUES ("Antalgiques", "Antalgiques");
-INSERT INTO classe_pharmacologique (code, parent) VALUES ("Antalgiques non opiacés", "Antalgiques");
-INSERT INTO classe_pharmacologique (code, parent) VALUES ("Endocrinologie", "Endocrinologie");
-INSERT INTO classe_pharmacologique (code, parent) VALUES ("Médicaments de la thyroïde", "Endocrinologie");
+INSERT INTO classe_pharmacologique (code, parent) VALUES ('Antalgiques', 'Antalgiques');
+INSERT INTO classe_pharmacologique (code, parent) VALUES ('Antalgiques non opiacés', 'Antalgiques');
+INSERT INTO classe_pharmacologique (code, parent) VALUES ('Endocrinologie', 'Endocrinologie');
+INSERT INTO classe_pharmacologique (code, parent) VALUES ('Médicaments de la thyroïde', 'Endocrinologie');
 
 
-INSERT INTO substance_pharmacologique (code, subs_id) VALUES ("Antalgiques non opiacés", 1);
-INSERT INTO substance_pharmacologique (code, subs_id) VALUES ("Antalgiques non opiacés", 2);
-INSERT INTO substance_pharmacologique (code, subs_id) VALUES ("Antalgiques non opiacés", 3);
-INSERT INTO substance_pharmacologique (code, subs_id) VALUES ("Médicaments de la thyroïde", 4);
+INSERT INTO substance_pharmacologique (code, subs_id) VALUES ('Antalgiques non opiacés', 1);
+INSERT INTO substance_pharmacologique (code, subs_id) VALUES ('Antalgiques non opiacés', 2);
+INSERT INTO substance_pharmacologique (code, subs_id) VALUES ('Antalgiques non opiacés', 3);
+INSERT INTO substance_pharmacologique (code, subs_id) VALUES ('Médicaments de la thyroïde', 4);
 
 
-INSERT INTO effet_connus_p (id, pharm_code) VALUES (1, "Antalgiques non opiacés");
-INSERT INTO effet_connus_p (id, pharm_code) VALUES (2, "Antalgiques non opiacés");
-INSERT INTO effet_connus_p (id, pharm_code) VALUES (3, "Antalgiques non opiacés");
-INSERT INTO effet_connus_p (id, pharm_code) VALUES (4, "Médicaments de la thyroïde");
+INSERT INTO effet_connus_p (id, pharm_code) VALUES (1, 'Antalgiques non opiacés');
+INSERT INTO effet_connus_p (id, pharm_code) VALUES (2, 'Antalgiques non opiacés');
+INSERT INTO effet_connus_p (id, pharm_code) VALUES (3, 'Antalgiques non opiacés');
+INSERT INTO effet_connus_p (id, pharm_code) VALUES (4, 'Médicaments de la thyroïde');
 
 
 INSERT INTO maladies (Code, Nom) VALUES('C', 'maladies')
@@ -78,9 +78,9 @@ INSERT INTO maladies (Code, Nom) VALUES('C02.081.343.340', 'encéphalite de cali
 INSERT INTO maladies (Code, Nom) VALUES('C02.081.343.345', 'encéphalite japonaise');
 INSERT INTO maladies (Code, Nom) VALUES('C02.081.343.350', 'encéphalite de Saint-Louis');
 INSERT INTO maladies (Code, Nom) VALUES('C02.081.355', 'encéphalomyélite équine');
-INSERT INTO maladies (Code, Nom) VALUES('C02.081.355.177', "encéphalomyélite équine de l'Est");
+INSERT INTO maladies (Code, Nom) VALUES('C02.081.355.177', 'encéphalomyélite équine de l Est');
 INSERT INTO maladies (Code, Nom) VALUES('C02.081.355.355', 'encéphalomyélite équine du vénézuéla');
-INSERT INTO maladies (Code, Nom) VALUES('C02.081.355.677', "encéphalomyélite équine de l'Ouest");
+INSERT INTO maladies (Code, Nom) VALUES('C02.081.355.677', 'encéphalomyélite équine de l Ouest');
 INSERT INTO maladies (Code, Nom) VALUES('C02.081', 'infections à arbovirus');
 INSERT INTO maladies (Code, Nom) VALUES('C02.081.030', 'peste équine');
 INSERT INTO maladies (Code, Nom) VALUES('C02.081.125', 'fièvre catarrhale du mouton');
@@ -92,20 +92,20 @@ INSERT INTO traitement (id, Date_debut, Date_fin, n_Med, n_pat) VALUES(2, TO_DAT
 INSERT INTO traitement (id, Date_debut, Date_fin, n_Med, n_pat) VALUES(3, TO_DATE('27/12/2029', 'DD/MM/YYYY'), TO_DATE('2/1/2030', 'DD/MM/YYYY'), 1, 2);
 
 
-INSERT INTO recommandation (id, text) VALUES (1, "Attention");
-INSERT INTO recommandation (id, text) VALUES (2, "Ne pas lancer les médicaments par terre trop violemment");
-INSERT INTO recommandation (id, text) VALUES (3, "Bien doser");
+INSERT INTO recommandation (id, text) VALUES (1, 'Attention');
+INSERT INTO recommandation (id, text) VALUES (2, 'Ne pas lancer les médicaments par terre trop violemment');
+INSERT INTO recommandation (id, text) VALUES (3, 'Bien doser');
 
 
 INSERT INTO consultation (id, n_med, n_pat, VisitDate) VALUES (1, 1, 1, TO_DATE('17/12/2015', 'DD/MM/YYYY'));
 INSERT INTO consultation (id, n_med, n_pat, VisitDate) VALUES (2, 2, 2, TO_DATE('19/12/2030', 'DD/MM/YYYY'));
 
 
-INSERT INTO observation (id, text) VALUES (1, "Toux");
-INSERT INTO observation (id, text) VALUES (2, "50° de fièvre");
-INSERT INTO observation (id, text) VALUES (3, "40° de fièvre");
-INSERT INTO observation (id, text) VALUES (4, "Oeil gonflé");
-INSERT INTO observation (id, text) VALUES (5, "Problème de tension");
+INSERT INTO observation (id, text) VALUES (1, 'Toux');
+INSERT INTO observation (id, text) VALUES (2, '50° de fièvre');
+INSERT INTO observation (id, text) VALUES (3, '40° de fièvre');
+INSERT INTO observation (id, text) VALUES (4, 'Oeil gonflé');
+INSERT INTO observation (id, text) VALUES (5, 'Problème de tension');
 
 
 INSERT INTO observe (obs_id, con_id) VALUES (1, 1);
