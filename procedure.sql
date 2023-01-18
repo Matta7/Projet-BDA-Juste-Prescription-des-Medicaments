6a1a1237d55265ec0e99bc63def00e5f9704f42b
SpiderInMyEar <93610647+SpiderInMyEar@users.noreply.github.com>
2023-01-18 19:01:46 +0100

    Update procedure.sql

--- procedure.sql
+++ procedure.sql
@@ -49,3 +49,50 @@ BEGIN
 	END LOOP;
 END;
 /
+
+CREATE OR REPLACE PROCEDURE side_effects_of(med_id medicament.id%TYPE) IS
+varx medicament.nom%TYPE;
+notice medicament.notice_url%TYPE;
+BEGIN
+	SELECT nom,notice_url INTO varx, notice
+	FROM medicament
+	WHERE id = med_id;
+	DBMS_OUTPUT.PUT_LINE('Known side effects of ' ||varx||' include:');
+	
+	FOR row IN (
+	SELECT *
+	FROM effet_indesirable ei
+	WHERE EXISTS (select * from effet_connus_m ec where ec.id = ei.id AND ec.medic_id = med_id)
+	) LOOP
+	DBMS_OUTPUT.PUT_LINE(row.text);
+	END LOOP;
+	
+	DBMS_OUTPUT.NEW_LINE;
+	DBMS_OUTPUT.PUT_LINE('    '||notice);
+	DBMS_OUTPUT.NEW_LINE;
+	DBMS_OUTPUT.PUT_LINE('Possible side effects include :');
+	
+	FOR row IN (
+	SELECT *
+	FROM effet_indesirable ei
+	WHERE NOT EXISTS (select * from effet_connus_m ec where ec.id = ei.id AND ec.medic_id = med_id)
+	AND (
+		EXISTS (select * from effet_connus_s ec where ec.id = ei.id AND ec.subs_id IN (
+			select sc.subs_id from substance_contenu sc where sc.medic_id = med_id))
+		OR EXISTS (select * from effet_connus_c ec where ec.id = ei.id AND ec.chim_code IN (
+			select c.code from classe_chimique c start with c.code in (select sc.code from substance_chimique sc
+				where sc.subs_id in (select scont.subs_id from substance_contenu scont where scont.medic_id = med_id)
+				) connect by prior parent = code
+			)
+		) OR EXISTS (select * from effet_connus_p ec where ec.id = ei.id AND ec.pharm_code IN (
+			select p.code from classe_pharmacologique p start with p.code in (select sp.code from substance_pharmacologique sp
+				where sp.subs_id in (select scont.subs_id from substance_contenu scont where scont.medic_id = med_id)
+				) connect by prior parent = code
+			)
+		)
+	)	
+	) LOOP
+	DBMS_OUTPUT.PUT_LINE(row.text);
+	END LOOP;
+END;
+/
